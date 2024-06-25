ca23f59f934bd18645af49d965dddb77e0fb797d
sparalaux <theodeletang@gmail.com>
2024-06-25 16:56:51 +0200

    ajout du deplacement du joueur

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+console.log("Js chargé")
+
+const joueur = document.querySelector(".joueur");
+
+const pos = {x:0, y:0};
+
+document.addEventListener("keydown", (ev) => {
+
+  const dir = (ev.key.match(/(?<=^Arrow)\w+/)||[])[0];
+  if (!dir) return; // Not an arrow key.
+  
+  ev.preventDefault(); // Prevent Browser scroll if overflow
+
+  ({
+    Left:  () => pos.x -= 5,
+    Right: () => pos.x += 5,
+    Up:    () => pos.y -= 5,
+    Down:  () => pos.y += 5,
+  }[dir])(); 
+  
+  joueur.style.transform = `translate(${pos.x}px, ${pos.y}px)`
+});
