c8ef0c3a048b13815d432412c57fa90aa5a16e3e
sparalaux <theodeletang@gmail.com>
2024-06-26 16:16:30 +0200

    ajout de l'apparition de poisson de maniere aleatoire

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,17 @@ function chronometre(){
 }
 
 document.addEventListener("onload",chronometre());
+
+function poisson(posx,posy){
+    const holdfish = document.querySelector(".holdfish");
+    holdfish.style.left = posx+"px";
+    holdfish.style.top = posy+"px";
+    holdfish.innerHTML = `<img class="poisson" src="./assets/images/poisson.png">`
+}
+
+// setInterval(() =>{
+//     var posx = Math.floor(Math.random() * largeecran);
+//     var posy = Math.floor(Math.random() * hautecran);
+//     console.log(posx, posy);
+//     poisson(posx,posy);
+// },1000);
