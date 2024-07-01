f3c11b3edef7467d064d9494f779fc5d2af17633
sparalaux <theodeletang@gmail.com>
2024-07-01 23:21:54 +0200

    ajout de la modal d'entré du jeu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,31 @@
 console.log("Js chargé")
 
-const joueur = document.querySelector(".joueur");
-pos = joueur.getBoundingClientRect();
-console.log(pos.top, pos.left, pos.bottom, pos.right);
+// Get the modal
+var modal = document.getElementById("myModal");
 
-const hautecran = window.innerHeight;
-const largeecran = window.innerWidth;
-console.log(hautecran, largeecran);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-
-document.addEventListener("keydown", (e) => {
-    const key = e.code;
-    console.log(key);
-    const cs = getComputedStyle(joueur);
-
-    e.preventDefault();
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    jeu();
+}
   
-    const change = 10;
-    const regex = /[\d\.]*/;
-  
-    const left = cs.left;
-    const top = cs.top;
-    const leftNumber = left.match(regex);
-    const topNumber = top.match(regex);
-  
-    // LEFT key pressed
-    if (key === "ArrowLeft" && leftNumber[0]>0) {
-      joueur.style.left = `${parseFloat(leftNumber[0]) - change}px`;
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      jeu();
     }
-    // TOP key pressed
-    if (key === "ArrowUp" && topNumber[0]>30) {
-        joueur.style.top = `${parseFloat(topNumber[0]) - change}px`;
-    }
-    // RIGHT key pressed
-    if (key === "ArrowRight" && leftNumber[0]<largeecran-100) {
-        joueur.style.left = `${parseFloat(leftNumber[0]) + change}px`;
-    }
-    // DOWN key pressed
-    if (key === "ArrowDown" && topNumber[0]<hautecran-120) {
-        joueur.style.top = `${parseFloat(topNumber[0]) + change}px`;
-    }
-  });
+}
+
+
+function start(){
+    modal.style.display = "block";
+}
+
+document.addEventListener("onload", start());
 
 const time = document.querySelector(".timer");
 // console.log(time);
@@ -54,7 +39,6 @@ function chronometre(){
     }, 1000);
 }
 
-document.addEventListener("onload",chronometre());
 const holdfish = document.querySelector(".holdfish");
 
 function poisson(posx,posy){
@@ -63,7 +47,53 @@ function poisson(posx,posy){
     holdfish.innerHTML = `<img class="poisson" src="./assets/images/poisson.png">`
 }
 
-poisson(0,50);
+
+function jeu(){
+    const joueur = document.querySelector(".joueur");
+    pos = joueur.getBoundingClientRect();
+    console.log(pos.top, pos.left, pos.bottom, pos.right);
+
+    const hautecran = window.innerHeight;
+    const largeecran = window.innerWidth;
+    console.log(hautecran, largeecran);
+
+
+    document.addEventListener("keydown", (e) => {
+        const key = e.code;
+        console.log(key);
+        const cs = getComputedStyle(joueur);
+
+        e.preventDefault();
+    
+        const change = 10;
+        const regex = /[\d\.]*/;
+    
+        const left = cs.left;
+        const top = cs.top;
+        const leftNumber = left.match(regex);
+        const topNumber = top.match(regex);
+    
+        // LEFT key pressed
+        if (key === "ArrowLeft" && leftNumber[0]>0) {
+        joueur.style.left = `${parseFloat(leftNumber[0]) - change}px`;
+        }
+        // TOP key pressed
+        if (key === "ArrowUp" && topNumber[0]>30) {
+            joueur.style.top = `${parseFloat(topNumber[0]) - change}px`;
+        }
+        // RIGHT key pressed
+        if (key === "ArrowRight" && leftNumber[0]<largeecran-100) {
+            joueur.style.left = `${parseFloat(leftNumber[0]) + change}px`;
+        }
+        // DOWN key pressed
+        if (key === "ArrowDown" && topNumber[0]<hautecran-120) {
+            joueur.style.top = `${parseFloat(topNumber[0]) + change}px`;
+        }
+    });
+    
+    chronometre();
+
+    poisson(0,50);
 
 // setInterval(() =>{
 //     var posx = Math.floor(Math.random() * largeecran);
@@ -71,3 +101,7 @@ poisson(0,50);
 //     // console.log(posx, posy);
 //     poisson(posx,posy);
 // },1000);
+}
+
+
+
